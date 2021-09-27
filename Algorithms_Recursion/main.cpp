c4fded48a885582bfb826156b01e363851752c2f
Shea <galley.shea@gmail.com>
2021-09-27 16:52:00 -0400

    Section 12: Recursion - finished

--- Algorithms_Recursion/main.cpp
+++ Algorithms_Recursion/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 
+#include <vector>
+vector<int> memo; //global vector of ints
+
 // 5! = 5*4*3*2*1 = 120						 Or 5! = 5 * 4!
 
 	int find_factorial_recursive(int num)
@@ -16,6 +19,45 @@ using namespace std;
 		cout << "calling factorial function again - recursive case" << endl;
 		return num*find_factorial_recursive(num-1); // multiply num by result of factorial function of num-1
 	}
+}
+
+// FIBONACCI 
+//Given a number N return the index value of the Fibonacci sequence, where the sequence is:
+
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 ...
+// the pattern of the sequence is that each value is the sum of the 2 previous values, that means that for N=5 → 2+3
+
+int fibonacci(int num)
+{
+	/*		GARBAGE
+	 * int num1{num-num};
+	int num2{num2*num};
+	int current_sum{num1+num2};
+	
+	
+	//base case
+	if (current_sum >= num)
+	{
+		return current_sum;
+	}
+	
+	num1 = num2;
+	num2 = current_sum;
+	//recursive case
+	return fibonacci_recursive(current_sum);
+	 * 
+	 * SOLUTION*/
+	 
+	 if (num == 0)
+        return 0;
+    if (num == 1)
+        return 1;
+    if (memo[num] != 0)
+        return memo[num];
+    memo[num] = fibonacci(num - 1) + fibonacci(num - 2);
+    return memo[num];
+	
+	
 }
 
 
